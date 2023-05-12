c90bad6f72680474478151a06ba0dfa3b8664716
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2023-05-12 21:49:11 +0630

    Array & object

--- function.js
+++ function.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
@@ -8,6 +10,12 @@ const functions = {
     user["lastName"] = "codeCamp";
     return user;
   },
+
+  fetchUser: () =>
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/1")
+      .then((res) => res.data)
+      .catch((err) => "error" + err),
 };
 
 module.exports = functions;
