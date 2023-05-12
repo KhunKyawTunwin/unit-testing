ed1994b28026ba7ddeb4ab085a1da9492fe69841
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2023-05-12 23:11:30 +0630

    finished Unittest

--- chunk.test.js
+++ chunk.test.js
@@ -18,3 +18,12 @@ test("Chunk an array of 10 values with length of 2", () => {
     [9, 10],
   ]);
 });
+
+test("Chunk an array of 3 values with length of 1", () => {
+  const numbers = [1, 2, 3];
+  const len = 1;
+
+  const chunkedArr = chunkArrayData(numbers, len);
+
+  expect(chunkedArr).toEqual([[1], [2], [3]]);
+});
